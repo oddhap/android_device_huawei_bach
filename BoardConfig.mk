1edf22da1d6dd9b4b013c617de3b6f8446144a2e
lineage-build <lineage@lineage-build.local>
2026-09-20 12:03:31 +0000

    bach: sepolicy: map dataservice_app to vendor_dataservice_app
    Change-Id: I5f9d552779d3b736198be546ac2b963ab1fefab6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,6 +226,8 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
+BOARD_SEPOLICY_M4DEFS += \
+    dataservice_app=vendor_dataservice_app
 
 # Shims
 TARGET_LD_SHIM_LIBS += \
