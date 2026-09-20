e49a251161ac6a74d8d3298e171cfd704ee1821f
lineage-build <lineage@lineage-build.local>
2026-09-20 10:38:00 +0000

    bach: gps: fix misleading indentation in battery_listener
    Change-Id: I1cd87f929b9c470401a64f4f8acf81dc4f49b9e4

--- gps/android/utils/battery_listener.cpp
+++ gps/android/utils/battery_listener.cpp
@@ -177,13 +177,13 @@ BatteryListenerImpl::~BatteryListenerImpl()
 {
     {
         std::lock_guard<std::mutex> _l(mLock);
-        if (mHealth != NULL)
-            mHealth->unlinkToDeath(this);
+        if (mHealth != NULL) {
             auto r = mHealth->unlinkToDeath(this);
             if (!r.isOk() || r == false) {
                 ALOGE("Transaction error in unregister to HealthHAL death: %s",
                         r.description().c_str());
             }
+        }
     }
     mDone = true;
     mThread->join();
