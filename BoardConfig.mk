0bfa650f65ceba7c38240f7c7f5ffd0fd3d61c41
lineage-build <lineage@lineage-build.local>
2026-09-20 15:19:37 +0000

    bach: sepolicy: label QCOM product libs as system_file
    Change-Id: I675ea8663ed9ed3f460a808e63c08863492246f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,6 +225,7 @@ BOARD_ROOT_EXTRA_SYMLINKS += \
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/product
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 SELINUX_IGNORE_NEVERALLOWS := true
 BOARD_SEPOLICY_M4DEFS += \
